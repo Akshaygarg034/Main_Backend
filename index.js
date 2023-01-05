4905aa58d085b05fc29ed6ecc923ceea5f90239a
Akshay <20bcs59@nith.ac.in>
2023-01-05 16:44:06 +0530

    main backend setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+var cors = require('cors');
+const connectToMongo = require('./db');
+const dotenv = require('dotenv');
+dotenv.config();
+connectToMongo();
+const app = express()
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+  res.status(300).send("Akshay's Main Backend is running properly");
+});
+
+// Recipe website
+app.use('/api/recipeauth', require('./routes/recipeauth'));
+app.use('/api/recipes', require('./routes/recipes'));
+app.use('/api/searchrecipes', require('./routes/searchrecipes.js'));
+// Notes website
+app.use('/api/noteauth', require('./routes/noteauth'));
+app.use('/api/notes', require('./routes/notes'));
+
+app.listen(port, () => {
+  console.log(`Main Backend listening on port ${port}`)
+})
